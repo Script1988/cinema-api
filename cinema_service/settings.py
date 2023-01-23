99d7c3e76dcf714e97c8953811ba5de230ade1b2
Volodymyr Panov <vpanov25@gmail.com>
2023-01-23 21:30:20 +0100

    Add description and access to readme.md

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -79,20 +79,14 @@ WSGI_APPLICATION = "cinema_service.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "DB_HOST": os.environ.get("POSTGRES_HOST"),
-        "DB_NAME": os.environ.get("POSTGRES_DB"),
-        "DB_USER": os.environ.get("POSTGRES_USER"),
-        "DB_PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+        "HOST": os.environ["POSTGRES_HOST"],
+        "NAME": os.environ["POSTGRES_DB"],
+        "USER": os.environ["POSTGRES_USER"],
+        "PASSWORD": os.environ["POSTGRES_PASSWORD"],
     }
 }
 
